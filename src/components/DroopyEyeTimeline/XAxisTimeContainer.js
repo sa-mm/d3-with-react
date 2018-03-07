5d3cf967d0e29778e1a4192aced464f8381c10d9
Sam <mcmyler@gmail.com>
2018-03-07 09:53:24 -0600

    Initial fix for timeline.

--- src/components/DroopyEyeTimeline/XAxisTimeContainer.js
+++ src/components/DroopyEyeTimeline/XAxisTimeContainer.js
@@ -1,10 +1,42 @@
 import React from "react";
 import { XAxisTime } from "../XAxisTime";
 
+import { timeParse } from "d3-time-format";
+import { isEqual, findIndex } from "lodash";
+
 class XAxisTimeContainer extends React.Component {
-  state = {
-    selectedEvent: 0
-  };
+  constructor(props) {
+    super(props);
+    const { scale, data } = props;
+    var parseTime = timeParse("%Y-%m-%d");
+
+    const ticksWithData = scale.ticks(data.length).reduce((acc, tick) => {
+      const matchedIndex = findIndex(data, e =>
+        isEqual(tick, parseTime(e.date))
+      );
+      if (matchedIndex >= 0) {
+        const { event } = data[matchedIndex];
+        return [...acc, { tick, event }];
+      }
+      return [...acc, { tick }];
+    }, []);
+
+    const dates = data.map(e => parseTime(e.date).toDateString());
+    const ticks = scale.ticks(data.length).map(e => e.toDateString());
+    const relTicks = ticks.reduce((acc, dateStr, i) => {
+      if (dates.some(e => e === dateStr)) {
+        return [...acc, { idx: i, str: dateStr }];
+      }
+      return acc;
+    }, []);
+
+    this.state = {
+      currentIndex: 0,
+      selectedEvent: 0,
+      relTicks,
+      ticksWithData
+    };
+  }
 
   handleEventClick = index => () => {
     if (this.eventInterval) clearInterval(this.eventInterval);
@@ -22,25 +54,27 @@ class XAxisTimeContainer extends React.Component {
   }
 
   incrementEvent = () => {
-    const { selectedEvent } = this.state;
-    const { scale, data } = this.props;
-    const atLastTick = scale.ticks(data.length).length - 1 === selectedEvent;
+    const { relTicks, currentIndex } = this.state;
+    const atLastTick = relTicks.length - 1 === currentIndex;
     if (atLastTick) {
       clearInterval(this.eventInterval);
     } else {
       this.setState((prevState, props) => {
-        const { selectedEvent } = prevState;
-        return { selectedEvent: selectedEvent + 1 };
+        const { relTicks, currentIndex } = prevState;
+        const nextIndex = currentIndex + 1;
+        const nextEvent = relTicks[nextIndex].idx;
+        return { selectedEvent: nextEvent, currentIndex: nextIndex };
       });
     }
   };
 
   render() {
-    const { selectedEvent } = this.state;
+    const { selectedEvent, ticksWithData } = this.state;
 
     return (
       <XAxisTime
         {...this.props}
+        ticksWithData={ticksWithData}
         selectedEvent={selectedEvent}
         handleEventClick={this.handleEventClick}
       />
--- src/components/XAxisTime.js
+++ src/components/XAxisTime.js
@@ -6,16 +6,13 @@ export const XAxisTime = ({
   transform = "",
   scale,
   data,
+  ticksWithData,
   accessor,
   width,
   height,
   selectedEvent,
   handleEventClick
 }) => {
-  const pathLine = line()
-    .x((d, i) => scale(d[accessor]))
-    .y(d => 0);
-
   const selectedEventStyle = {
     fill: "url(#PhoenixStart)"
   };
@@ -29,38 +26,34 @@ export const XAxisTime = ({
         x2={width}
         y2="0.001"
       />
-      {scale.ticks(data.length).map((tick, i) => {
+      {ticksWithData.map(({ tick, event }, i) => {
         const xCoordinate = scale(tick);
-        const dates = Object.values(data).map(e =>
-          new Date(e.date).toDateString()
-        );
-        const relIdx = dates.findIndex(
-          dateStr => dateStr === tick.toDateString()
-        );
         return (
-          <g key={`x-axis-tick-${i}`} fontSize={1}>
-            <g transform={`translate(${width / 2},${-height / 4})`}>
-              <text
-                textAnchor="middle"
-                style={{ display: selectedEvent === i ? "initial" : "none" }}
-              >
-                {relIdx > 0 && data[relIdx].event}
-              </text>
+          event && (
+            <g key={`x-axis-tick-${i}`} fontSize={1}>
+              <g transform={`translate(${width / 2},${-height / 4})`}>
+                <text
+                  textAnchor="middle"
+                  style={{ display: selectedEvent === i ? "initial" : "none" }}
+                >
+                  {event || null}
+                </text>
+              </g>
+              <g transform={`translate(${xCoordinate},0)`}>
+                <circle
+                  style={selectedEvent === i ? selectedEventStyle : {}}
+                  cx="0"
+                  cy="0"
+                  r="2"
+                  fill="url(#SnowAgain)"
+                  onClick={handleEventClick(i)}
+                />
+                <text dy={3} dx={2.5} transform={`rotate(45)`}>
+                  {tick.toDateString()}
+                </text>
+              </g>
             </g>
-            <g transform={`translate(${xCoordinate},0)`}>
-              <circle
-                style={selectedEvent === i ? selectedEventStyle : {}}
-                cx="0"
-                cy="0"
-                r="2"
-                fill="url(#SnowAgain)"
-                onClick={handleEventClick(i)}
-              />
-              <text dy={3} dx={2.5} transform={`rotate(45)`}>
-                {tick.toDateString()}
-              </text>
-            </g>
-          </g>
+          )
         );
       })}
     </g>
